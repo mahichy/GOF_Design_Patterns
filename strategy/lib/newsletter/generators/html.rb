a33aaa49cb862a504f27adf382fb3720d61b9f8b
mahi <mohiddinoshim@yahoo.com>
2020-11-26 23:12:10 +0600

    part_one with problem

---
+++ strategy/lib/newsletter/generators/html.rb
@@ -0,0 +1,25 @@
+module Newsletter
+	module Generators
+		class HTML
+			def execute
+			  <<EOF
+#{header}
+
+#{content}
+EOF
+				
+			end
+			def header
+				"<h1>Hello World!</h1>"
+			end
+
+			def content
+				<<EOF
+<div class="content">
+  <p>lorem ipsum</p>
+</div>
+EOF
+			end
+		end
+	end
+end
---
+++ strategy/test/template_method_test.rb
@@ -0,0 +1,33 @@
+require 'minitest/autorun'
+require_relative "../lib/newsletter/generators/base"
+require_relative "../lib/newsletter/generators/html"
+require_relative "../lib/newsletter/generators/markdown"
+
+module Newsletter
+	describe Generators::Base do
+		it "generators a newsletter in HTML" do
+			final_result = File.read(File.expand_path(
+				"fixtures/newsletter.html",
+				File.dirname(__FILE__)
+			))
+
+			Generators::Base.new(:html).render.must_include(final_result)
+		end
+
+		it "generate a newsletter in HTML" do
+			skip
+			final_result = File.read(File.expand_path(
+				"fixtures/newsletter.markdown",
+				File.dirname(__FILE__)
+			))
+
+			Generators::Base.new(:markdown).render.must_include(final_result)
+		end
+
+		it "fails to render if calling the base generator class" do
+			skip
+			-> { Generators.new.header }.must_raise NotImplementedError
+			-> { Generators.new.content }.must_raise NotImplementedError
+		end
+	end
+end
