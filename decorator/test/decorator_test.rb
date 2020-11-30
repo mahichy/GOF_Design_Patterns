9836cc0de17098eb12aaf553f30ddfbe2b5842b6
mahi <mohiddinoshim@yahoo.com>
2020-11-30 21:57:25 +0600

    problem_01

---
+++ decorator/test/decorator_test.rb
@@ -0,0 +1,26 @@
+require 'minitest/autorun'
+require_relative "../lib/product"
+
+describe Product do
+	let(:product) {Product.new("Tomato", :fruit, [ %w(red salad sour) ]) }
+	let(:expected_result) { <<EOF
+		<tr>
+		<th>Tomato</th>
+		<td>fruit</td>
+		<td>
+			<ul>
+				<li>red</li>
+				<li>salad</li>
+				<li>sour</li>
+			</ul>
+		</td>
+		</tr>
+EOF
+	}
+	it "can be printed as a HTML table row" do
+		product.as_html_row.
+			strip.gsub(/(^\s+)|\n/, ""). # remove all indentation and newlines
+			must_equal expected_result.
+			strip.gsub /(^\s+)|\n/, ""   # remove all indentation and newlines
+	end
+end
