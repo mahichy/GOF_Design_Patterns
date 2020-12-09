86a61e76150fbba9f5466aa1a615186cde825e91
mahi <mohiddinoshim@yahoo.com>
2020-12-09 16:23:17 +0600

    Ranges done

--- builder/lib/rental_plan.rb
+++ builder/lib/rental_plan.rb
@@ -3,17 +3,24 @@ require_relative "rental_plan/day_range"
 require_relative "rental_plan/hour_range"
 
 class RentalPlan
-	def self.build naem
+	def self.build name
 		build_metadata name
-		new @metadata
+		build_list_of_ranges
+		new @metadata, @ranges
 	end
 
 	def self.build_metadata name
 		@metadata = Metadata.new name
 	end
-
-	def initialize metadata
+	attr_reader :ranges
+	
+	def initialize metadata, ranges
 		@metadata = metadata
+		@ranges = ranges
+	end
+
+	def self.build_list_of_ranges
+		@ranges = [ DayRange.new ]
 	end
 
 	def name
