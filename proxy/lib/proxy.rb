9230b3c0a9f25c62f82652fdd2d8973d9337e307
mahi <mohiddinoshim@yahoo.com>
2020-12-17 12:17:21 +0600

    Proxy Pattern done

--- proxy/lib/proxy.rb
+++ proxy/lib/proxy.rb
@@ -1,10 +1,15 @@
 class VideoAuthenticationProxy
-	def initialize video
+	def initialize video, user
 		@video = video
+		@user = user
 	end
 
 	def play
-		@video.play
+		if @user == "Privileged"
+			@video.play
+		else
+			raise ForbidenAccessError, "Only Privileged users are allowed."
+		end
 	end
 
 end
--- proxy/test/proxy_pattern_test.rb
+++ proxy/test/proxy_pattern_test.rb
@@ -2,18 +2,22 @@ require "minitest/autorun"
 require_relative "../lib/proxy"
 require_relative "../lib/video"
 
+class ForbidenAccessError < StandardError; end
+
 describe VideoAuthenticationProxy do
 	it "allows priviliged users to access the video" do
 		video = Video.new "My first video", "My first description"
-		proxy = VideoAuthenticationProxy.new video 
-		proxy.play
-		
-		capture_io { video.play }.to_s.must_include "My first video"
-		capture_io { video.play }.to_s.must_include "My first description"
+		proxy = VideoAuthenticationProxy.new video, "Privileged" 
+
+		capture_io { proxy.play }.to_s.must_include "My first video"
+		capture_io { proxy.play }.to_s.must_include "My first description"
 	end
 	
 	it "forbids regular users to access the videos" do
-		skip
+		video = Video.new "My first video", "My first description"
+		proxy = VideoAuthenticationProxy.new video, "Anonymous" 
+
+		-> { proxy.play }.must_raise ForbidenAccessError
 	end	
 		
 		
