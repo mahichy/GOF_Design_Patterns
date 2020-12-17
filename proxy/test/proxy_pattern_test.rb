dd2fc960a0db0ebc63b096e663bad29d8b54835d
mahi <mohiddinoshim@yahoo.com>
2020-12-17 10:38:33 +0600

    part two done

---
+++ proxy/test/proxy_pattern_test.rb
@@ -0,0 +1,12 @@
+require "minitest/autorun"
+require_relative "../lib/proxy"
+require_relative "../lib/video"
+
+describe VideoAuthenticationProxy do
+	it "allows priviliged users to access the video" do
+	end
+	
+	it "forbids regular users to access the videos" do
+		skip
+	end	
+end
